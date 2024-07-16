a25263e5ff3690e425498f748c43b74816da8ad9
Tino <121829394+faffietheripper@users.noreply.github.com>
2024-07-16 23:57:03 +0100

    set up database and shadcn ui

---
+++ drizzle.config.ts
@@ -0,0 +1,23 @@
+import { env } from "@/env";
+import { defineConfig } from "drizzle-kit";
+
+import { loadEnvConfig } from "@next/env";
+
+const projectDir = process.cwd();
+loadEnvConfig(projectDir);
+
+const DATABASE_URL = env.DATABASE_URL;
+if (!DATABASE_URL) {
+  throw new Error("POSTGRES_URL is not set");
+}
+
+export default defineConfig({
+  schema: "./src/db/schema.ts",
+  dialect: "postgresql",
+  out: "./drizzle",
+  dbCredentials: {
+    url: DATABASE_URL,
+  },
+  verbose: true,
+  strict: true,
+});
