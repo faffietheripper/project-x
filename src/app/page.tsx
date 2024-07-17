f6da110452767769900a138294a02a6462a4e616
Tino <121829394+faffietheripper@users.noreply.github.com>
2024-07-17 02:24:02 +0100

    added next-auth and items table

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,31 +1,48 @@
 import React from "react";
 import { database } from "@/db/database";
-import { bids as bidsSchema } from "@/db/schema";
+import { bids as bidsSchema, items } from "@/db/schema";
 import { Button } from "@/components/ui/button";
 import { revalidatePath } from "next/cache";
+import { SignIn } from "@/components/sign-in";
+import { SignOut } from "@/components/sign-out";
+import { auth } from "@/auth";
 
 export default async function HomePage() {
-  const bids = await database.query.bids.findMany();
+  const session = await auth();
+
+  const allItems = await database.query.items.findMany();
+
+  if (!session) return null;
+  const user = session.user;
+  if (!user) return null;
 
   return (
     <main>
+      {session ? <SignOut /> : <SignIn />}
+
+      {session?.user?.name}
+
       <form
         action={async (formData: FormData) => {
           "use server";
-          await database?.insert(bidsSchema).values({});
+          await database?.insert(items).values({
+            name: formData.get("name") as string,
+            userId: session?.user?.id!,
+          });
           revalidatePath("/");
         }}
       >
-        <input name="bid" placeholder="Bid" />
-        <button type="submit">Place Bid</button>
+        <input
+          name="name"
+          placeholder="Name your item"
+          className="text-black"
+        />
+        <Button type="submit">Post Item</Button>
       </form>
 
-      {bids.map((bid) => (
-        <div key={bid.id}>{bid.id}</div>
+      {allItems.map((item) => (
+        <div key={item.id}>{item.name}</div>
       ))}
-      <div>
-        <Button>Click me</Button>
-      </div>
     </main>
   );
 }
